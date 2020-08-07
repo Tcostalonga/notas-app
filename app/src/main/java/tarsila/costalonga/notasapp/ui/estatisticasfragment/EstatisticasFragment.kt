74a22006feed2a850a35da8f01a62dd07b65d024
Társila Costalonga <tarsila.costalonga@gmail.com>
2020-08-07 07:41:52 -0300

    Adição do estatisticasFragment, add de anexo no AddFragment

---
+++ app/src/main/java/tarsila/costalonga/notasapp/ui/estatisticasfragment/EstatisticasFragment.kt
@@ -0,0 +1,27 @@
+package tarsila.costalonga.notasapp.ui.estatisticasfragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import tarsila.costalonga.notasapp.R
+import tarsila.costalonga.notasapp.databinding.FragmentEstatisticasBinding
+
+
+class EstatisticasFragment : Fragment() {
+
+    private lateinit var binding: FragmentEstatisticasBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+     binding = DataBindingUtil.inflate(inflater, R.layout.fragment_estatisticas, container, false)
+
+        return binding.root
+    }
+
+}
