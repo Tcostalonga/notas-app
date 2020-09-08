135ba5134f0991df528afe790691a1fdb3711bfc
Társila Costalonga <tarsila.costalonga@gmail.com>
2020-09-08 20:26:52 -0300

    Add arquivo README; Teste para banco de dados e inserção de nota

---
+++ app/README-Template.md
@@ -0,0 +1,59 @@
+# notaAnota
+ 
+ 
+## Introdução
+
+Aplicativo para criação e controle de anotações.
+
+Funcionalidades:
+
+- Criar, editar e remover uma anotação;
+
+- Marcar uma nota como finalizada (através de um checkbox);
+
+- Exibir a data e hora em que uma nota foi criada e atualizada por último;
+
+- Compartilhar uma nota;
+
+- Exibir estatísticas com o total de notas criadas, ativas e finalizadas;
+
+- Trocar tema light e dark do app.
+
+
+### Pré-requisitos
+
+Para instalar o app: Smarthphone: Android 6.0 - Marshmallow - API 23
+
+Para executar o código: 
+- Andorid Studio: 4.0
+- Gradle: 4.0
+
+
+## Imagens
+
+
+
+## Construído com:
+
+* [Kotlin](https://kotlinlang.org/) - Linguagem de programação orientada a objetos
+* [MVVM](https://developer.android.com/jetpack/guide) - Padrão de projeto
+* [Room](https://developer.android.com/topic/libraries/architecture/room) - Biblioteca de persistência de dados
+* [Coroutines](https://developer.android.com/kotlin/coroutines) - Solução para programação assíncrona no Android
+* [Hilt](https://developer.android.com/training/dependency-injection/hilt-android) - Biblioteca de injeção de dependência
+* [LiveData](https://developer.android.com/topic/libraries/architecture/livedata) - Classe armazenadora de dados observáveis
+* [Navigation](https://developer.android.com/guide/navigation/navigation-getting-started) - Framework para navegar entre destinos em um aplicativo
+* [Constraint Layout](https://developer.android.com/training/constraint-layout) - ViewGroup que permite 
+* [Material Design](https://material.io/) - Sistema de design de UI 
+* [Data Binding](https://developer.android.com/topic/libraries/data-binding) - Biblioteca para vinculação de componentes de UI a fontes de dados
+* [Shared Preferences](https://developer.android.com/training/data-storage/shared-preferences) - API de persistência de dados em key-value par.
+* [Flipper]( https://fbflipper.com/) -  Plataforma de debug
+* [Espresso](https://developer.android.com/studio/test/espresso-test-recorder?hl=pt-br) - API do AndroidX Test
+* [Truth](https://truth.dev/) - Biblioteca para realziar asserções em testes
+
+
+## Autora
+
+* **Társila Costalonga** - [GitHub](https://github.com/Tcostalonga)
+
+ 
+
---
+++ app/src/androidTest/java/tarsila/costalonga/notasapp/bd/NotasDaoTest.kt
@@ -0,0 +1,88 @@
+package tarsila.costalonga.notasapp.bd
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Root
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import tarsila.costalonga.notasapp.getOrAwaitValue
+
+@ExperimentalCoroutinesApi
+@RunWith(JUnit4::class)
+@SmallTest
+class NotasDaoTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+
+    private lateinit var database: NotasRoom
+    private lateinit var dao: NotasDao
+
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            NotasRoom::class.java
+        ).allowMainThreadQueries().build()
+        dao = database.notasDao
+
+    }
+
+
+    @After
+    fun teardown() {
+        database.close()
+    }
+
+
+    @Test
+    fun insertNota() = runBlockingTest {
+        val notaItem =
+            Notas(id = 1, titulo = "Teste de titulo", anotacao = "Descrição da nota", ordem = 1)
+        dao.insertNota(notaItem)
+
+        val allNotasLD = dao.getAllNotas().getOrAwaitValue()
+
+        assertThat(allNotasLD).contains(notaItem)
+    }
+
+
+    @Test
+    fun deleteNota() = runBlockingTest {
+        val notaItem =
+            Notas(id = 1, titulo = "Teste de titulo", anotacao = "Descrição da nota", ordem = 1)
+        dao.insertNota(notaItem)
+
+        dao.deleteUmaNota(notaItem)
+
+        val allNotasLD = dao.getAllNotas().getOrAwaitValue()
+
+        assertThat(allNotasLD).doesNotContain(notaItem)
+    }
+
+
+    @Test
+    fun updateNota() = runBlockingTest {
+        val notaItem =
+            Notas(id = 1, titulo = "Teste de titulo", anotacao = "Descrição da nota", ordem = 1)
+        dao.insertNota(notaItem)
+
+        notaItem.titulo = "Titulo dois"
+        dao.updateNota(notaItem)
+
+        assertThat(notaItem.titulo).matches("Titulo dois")
+
+    }
+
+
+}
