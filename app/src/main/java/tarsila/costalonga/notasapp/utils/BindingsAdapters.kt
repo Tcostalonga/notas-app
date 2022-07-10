8796a858fc27d32295930e0044efda129c8ab771
Tarsila Costalonga <tarsila.costalonga@hotmart.com>
2022-07-10 18:32:03 -0300

    110722 - Concluir remoção de data binding do projeto

--- app/src/main/java/tarsila/costalonga/notasapp/utils/BindingsAdapters.kt
+++ app/src/main/java/tarsila/costalonga/notasapp/utils/BindingsAdapters.kt
@@ -2,13 +2,11 @@ package tarsila.costalonga.notasapp.utils
 
 import android.graphics.Paint
 import android.widget.TextView
-import androidx.databinding.BindingAdapter
 
-@BindingAdapter("app:notaFinalizada")
 fun TextView.setHachura(enabled: Boolean) {
-    if (enabled) {
-        paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+    paintFlags = if (enabled) {
+        paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
     } else {
-        paintFlags = paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+        paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
     }
 }
