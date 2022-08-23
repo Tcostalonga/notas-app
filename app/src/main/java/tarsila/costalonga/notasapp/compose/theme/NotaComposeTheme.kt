55e68eabd9bea1abc79de4b58165e0e8388e3e78
Tarsila Costalonga <tarsila.costalonga@hotmart.com>
2022-08-23 09:29:39 -0300

    110722 - Ajustes no tema e dimens

--- app/src/main/java/tarsila/costalonga/notasapp/compose/theme/NotaComposeTheme.kt
+++ app/src/main/java/tarsila/costalonga/notasapp/compose/theme/NotaComposeTheme.kt
@@ -1,5 +1,6 @@
 package tarsila.costalonga.notasapp.compose.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
@@ -8,12 +9,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import tarsila.costalonga.notasapp.R
 
 @Composable
-fun NotaComposeTheme(content: @Composable () -> Unit) {
-    MaterialTheme(colors = LightColors, typography = NotaTypography, content = content)
+fun NotaComposeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkColors else LightColors,
+        typography = NotaTypography,
+        content = content
+    )
 }
 
 private val LightColors = lightColors(
@@ -37,13 +46,25 @@ private val DarkColors = darkColors(
 )
 
 private val ProximaNova = FontFamily(
-    Font(R.font.proxima_nova_regular),
-    Font(R.font.proxima_nova)
+    Font(R.font.proxima_nova_regular, FontWeight.Normal)
 )
 
-private val NotaTypography = Typography(
+private val ProximaNovaCondensed = FontFamily(
+    Font(R.font.proxima_nova, FontWeight.Normal)
+)
+
+val NotaTypography = Typography(
     body1 = TextStyle(
         fontFamily = ProximaNova,
         fontSize = 18.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = ProximaNovaCondensed,
+        fontSize = 18.sp
+    ),
+    overline = TextStyle(
+        fontFamily = ProximaNovaCondensed,
+        color = colorPrimaryDarkLight,
+        fontSize = 14.sp
     )
 )
