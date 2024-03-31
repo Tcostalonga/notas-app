3113d23dc092ccdeb1226129c15a9af4a5473560
Tarsila Costalonga <tarsila.costalonga@gmail.com>
2024-03-30 22:12:42 -0300

    Migrate gradle from groovy to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,105 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlinParcelize)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.navigationSafeargs)
+}
+
+android {
+    namespace = "tarsila.costalonga.notasapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "tarsila.costalonga.notasapp"
+        minSdk = 23
+        targetSdk = 33
+        versionCode = 11
+        versionName = "1.1-jetpack"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+        compose = true
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xjvm-default=all")
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion =
+            extensions.getByType<VersionCatalogsExtension>().named("libs")
+                .findVersion("composeCompiler").get().toString()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    debugImplementation(libs.compose.ui.tooling.preview)
+
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.compiler)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.fragment)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.lifecycle.livedata)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.runtime.livedata)
+    implementation(libs.androidx.ui.viewbinding)
+    implementation(libs.compose.theme.adapter)
+    implementation(libs.core.test)
+    implementation(libs.hilt.android)
+    implementation(libs.kotlin.stdlib.jdk8)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.material)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.foundation.layout)
+    implementation(libs.compose.material)
+    implementation(libs.compose.runtime)
+    implementation(libs.compose.tooling.preview)
+    implementation(libs.compose.ui)
+    implementation(platform(libs.compose.bom))
+
+    ksp(libs.androidx.room.compiler)
+    ksp(libs.hilt.android.compiler)
+
+    testImplementation(libs.junit)
+
+}
