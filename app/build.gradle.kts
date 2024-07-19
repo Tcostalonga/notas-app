c00426575bc0bd3aa27e4599b5d391d3967f1bd6
Tarsila Costalonga <tarsila.costalonga@gmail.com>
2024-07-19 16:58:06 -0300

    Update gradle to 8.5.1 and other dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.hilt)
     alias(libs.plugins.kotlinParcelize)
     alias(libs.plugins.kotlinAndroid)
