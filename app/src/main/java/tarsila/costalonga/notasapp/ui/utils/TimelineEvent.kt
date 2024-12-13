977d9112f578d2e76d64b59978090db9826069b6
TCostalonga <tarsila.costalonga@gmail.com>
2024-12-12 21:28:12 -0300

    Add data class for TimelineEvent

---
+++ app/src/main/java/tarsila/costalonga/notasapp/ui/utils/TimelineEvent.kt
@@ -0,0 +1,9 @@
+package tarsila.costalonga.notasapp.ui.utils
+
+import androidx.annotation.StringRes
+
+data class TimelineEvent(
+    val id: Int,
+    @StringRes val date: Int,
+    @StringRes val description: Int,
+)
