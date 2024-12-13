66f18db6d9b2d9475ed6a9df9fc33af66d9d63e7
TCostalonga <tarsila.costalonga@gmail.com>
2024-12-12 21:28:55 -0300

    Wip: Implement list

--- app/src/main/java/tarsila/costalonga/notasapp/ui/about/compose/AboutScreen.kt
+++ app/src/main/java/tarsila/costalonga/notasapp/ui/about/compose/AboutScreen.kt
@@ -1,13 +1,42 @@
 package tarsila.costalonga.notasapp.ui.about.compose
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -18,11 +47,13 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withLink
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import tarsila.costalonga.notasapp.R
 import tarsila.costalonga.notasapp.ui.core.compose.MyTopAppBar
 import tarsila.costalonga.notasapp.ui.core.compose.theme.NotaComposeTheme
 import tarsila.costalonga.notasapp.ui.core.compose.theme.NoteTheme
+import tarsila.costalonga.notasapp.ui.utils.AboutListFactory
 
 private const val LINK1_WORD = "\nLinkedIn"
 private const val LINK2_WORD = "\nEmail"
@@ -31,27 +62,41 @@ private const val LINK2 = "mailto:tarsila.costalonga@gmail.com"
 
 @Composable
 internal fun AboutScreen() {
+    val list = AboutListFactory().generateListOfTimelineEvents()
     Scaffold(topBar = { MyTopAppBar() }) {
         Column(
             modifier = Modifier
-                .padding(horizontal = NoteTheme.spacing.spacer16)
-                .padding(it),
+                .padding(it)
+                .padding(NoteTheme.spacing.spacer16),
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.icon_splash),
-                contentDescription = null,
-                modifier =
-                Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(vertical = NoteTheme.spacing.spacer16)
-                    .background(
-                        NoteTheme.colors.primaryContainer,
-                        shape = RoundedCornerShape(percent = 100),
-                    ),
-            )
+            LazyColumn {
+                item {
+                    Column {
+                        Image(
+                            painter = painterResource(id = R.drawable.icon_splash),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .padding(vertical = NoteTheme.spacing.spacer16)
+                                .background(
+                                    NoteTheme.colors.primaryContainer,
+                                    shape = RoundedCornerShape(percent = 100),
+                                ),
+                        )
+                        LinkableText()
 
-            LinkableText()
+                        Spacer(modifier = Modifier.size(NoteTheme.spacing.spacer16))
+                    }
+                }
+                items(list, key = { item -> item.id }) { item ->
+                    TimelineWithProgress(
+                        date = stringResource(id = item.date),
+                        description = stringResource(id = item.description),
+                    )
+                }
+            }
         }
+
     }
 }
 
@@ -79,10 +124,70 @@ fun LinkableText() {
     )
 }
 
-@PreviewLightDark
+@Preview(showBackground = true)
 @Composable
 fun PreviewAbout() {
     NotaComposeTheme {
         AboutScreen()
     }
 }
+
+@Composable
+fun TimelineWithProgress(
+    date: String,
+    description: String,
+    modifier: Modifier = Modifier,
+) {
+    var isExpanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+            ) {
+                isExpanded = !isExpanded
+            }
+            .padding(vertical = NoteTheme.spacing.spacer4),
+    ) {
+        Column(
+            Modifier.fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(painterResource(R.drawable.icon_dot), null, tint = NoteTheme.colors.primary)
+            VerticalDivider(
+                modifier = Modifier
+                    .animateContentSize()
+                    .then(
+                        if (isExpanded) {
+                            Modifier.fillMaxHeight()
+                        } else {
+                            Modifier.height(40.dp)
+                        },
+                    )
+                    .width(NoteTheme.spacing.spacer1)
+                    .background(NoteTheme.colors.primary),
+            )
+        }
+        Column(
+            modifier = Modifier.weight(1f),
+        ) {
+            Text(
+                text = date,
+                style = NoteTheme.typography.labelLarge,
+            )
+            if (isExpanded) {
+                Text(
+                    text = description,
+                    style = NoteTheme.typography.labelSmall,
+                    modifier = Modifier.padding(vertical = NoteTheme.spacing.spacer8)
+                )
+            }
+        }
+    }
+}
+
+
+
