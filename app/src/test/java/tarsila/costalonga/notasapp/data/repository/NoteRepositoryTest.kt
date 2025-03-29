b86404733eca1c7e2aaa04052ff1ce7b42e7b639
TCostalonga <tarsila.costalonga@gmail.com>
2025-03-29 14:52:02 -0300

    Add NoteRepositoryTest

---
+++ app/src/test/java/tarsila/costalonga/notasapp/data/repository/NoteRepositoryTest.kt
@@ -0,0 +1,135 @@
+package tarsila.costalonga.notasapp.data.repository
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.just
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import tarsila.costalonga.notasapp.data.local.Note
+import tarsila.costalonga.notasapp.data.local.NoteDao
+import tarsila.costalonga.notasapp.rules.InstantTaskRule
+
+class NoteRepositoryTest {
+
+    @get:Rule
+    val instantTaskRule = InstantTaskRule()
+
+    private val dao: NoteDao = mockk()
+
+    private lateinit var repository: NoteRepository
+
+    private val stubNote = Note(
+        id = 3777,
+        title = "Lorem ipsum dolor",
+        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, ",
+        createdAt = 6389,
+        updatedAt = 3947,
+        isFinished = false,
+        sort = 1782,
+    )
+
+    @Before
+    fun setUp() {
+        repository = NoteDataRepository(dao)
+    }
+
+    @Test
+    fun `should insertNote runs successfully`() = runTest {
+        coEvery { dao.insertNota(any()) } just Runs
+
+        repository.insertNota(stubNote)
+
+        coVerify(exactly = 1) { dao.insertNota(stubNote) }
+    }
+
+    @Test
+    fun `should updateNote runs successfully`() = runTest {
+        coEvery { dao.updateNota(any()) } just Runs
+
+        repository.updateNota(stubNote)
+
+        coVerify(exactly = 1) { dao.updateNota(stubNote) }
+    }
+
+    @Test
+    fun `should deleteNote runs successfully`() = runTest {
+        coEvery { dao.deleteUmaNota(any()) } just Runs
+
+        repository.deleteUmaNota(stubNote)
+
+        coVerify(exactly = 1) { dao.deleteUmaNota(stubNote) }
+    }
+
+    @Test
+    fun `should getAllNotes runs successfully`() = runTest {
+        val expected = flowOf(listOf(stubNote))
+        coEvery { dao.getAllNotes() } returns expected
+
+        val result = repository.getAllNotes()
+
+        coVerify(exactly = 1) { dao.getAllNotes() }
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should getNoteById runs successfully`() = runTest {
+        val expected = flowOf(listOf(stubNote))
+        coEvery { dao.getAllNotes() } returns expected
+
+        val result = repository.getAllNotes()
+
+        coVerify(exactly = 1) { dao.getAllNotes() }
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should getDoneNotes runs successfully`() = runTest {
+        val expected = flowOf(listOf(stubNote))
+        coEvery { dao.getAllNotes() } returns expected
+
+        val result = repository.getAllNotes()
+
+        coVerify(exactly = 1) { dao.getAllNotes() }
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should getActiveNotes runs successfully`() = runTest {
+        val expected = 4
+        coEvery { dao.getActiveNotes() } returns expected
+
+        val result = repository.getActiveNotes()
+
+        coVerify(exactly = 1) { dao.getActiveNotes() }
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should getLastItemId runs successfully`() = runTest {
+        val expected = 123L
+        coEvery { dao.getLastItemId() } returns expected
+
+        val result = repository.getLastItemId()
+
+        coVerify(exactly = 1) { dao.getLastItemId() }
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should getNotesCount runs successfully`() = runTest {
+        val expected = 5
+        coEvery { dao.getNotesCount() } returns expected
+
+        val result = repository.getNotesCount()
+
+        coVerify(exactly = 1) { dao.getNotesCount() }
+        assertThat(result).isEqualTo(expected)
+    }
+}
