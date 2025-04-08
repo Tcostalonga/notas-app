f80f9de3f1b8e5aceb92d1c9d60660515441f350
TCostalonga <tarsila.costalonga@gmail.com>
2025-04-07 21:10:38 -0300

    Add timeline event

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,12 +65,6 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-
-    kotlin {
-        sourceSets.all {
-            languageSettings.enableLanguageFeature("ExplicitBackingFields")
-        }
-    }
 }
 
 dependencies {
