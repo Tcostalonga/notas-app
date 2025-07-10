76f21fec81320026f161ecc457dff06030950619
TCostalonga <tarsila.costalonga@gmail.com>
2025-07-09 21:15:30 -0300

    Update skd36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,14 +10,14 @@ plugins {
 
 android {
     namespace = "tarsila.costalonga.notasapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "tarsila.costalonga.notasapp"
         minSdk = 23
-        targetSdk = 35
-        versionCode = 14
-        versionName = "3.1-compose"
+        targetSdk = 36
+        versionCode = 15
+        versionName = "4-compose"
         vectorDrawables {
             useSupportLibrary = true
         }
